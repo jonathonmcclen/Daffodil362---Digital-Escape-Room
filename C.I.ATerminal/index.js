37a55fde1e6c74388190999bdafe7d87676702ab
--replace-all <jonathonsmcclendon@gmail.com>
2022-03-18 15:07:05 -0500

    index.js is fully cleaned

--- C.I.ATerminal/index.js
+++ C.I.ATerminal/index.js
@@ -1,23 +1,17 @@
 //
 //
 //
-var inputAllowed = true;
-const ENTER = 13;
-var ENTER_DOWN = false;
-
+// START UP
 window.onload = function () {
   document.addEventListener("keydown", keyPressed);
   document.addEventListener("keyup", keyReleased);
   //create playTimer
 };
 
-function grabSub() {
-  playerTXT = document.getElementById("txt").value;
-}
-
-function clearTxtBox() {
-  document.getElementById("txt").value = "";
-}
+// HANDLE ENTER KEY PRESS
+const ENTER = 13;
+var ENTER_DOWN = false;
+var inputAllowed = true;
 
 function keyPressed(evt) {
   let button_beep = document.createElement("audio");
@@ -41,55 +35,17 @@ function keyReleased(evt) {
   }
 }
 
+function grabSub() {
+  playerTXT = document.getElementById("txt").value;
+}
+
+function clearTxtBox() {
+  document.getElementById("txt").value = "";
+}
+
 function scrollToBottom() {
   window.scrollTo(
     0,
     document.body.scrollHeight || document.documentElement.scrollHeight
   );
-}
-
-function addNewLogItem(user, txt, blue = false) {
-  if (user == "player") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = "C:\\User\\Input> " + txt;
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  } else if (user == "comp") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = "C: Output> " + txt;
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  } else if (user == "") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = txt;
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  } else if (user == "loading") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = txt;
-    btn.className = "temp";
-    btn.id = "temp";
-    //myPara.setAttribute("id", "id_you_like");
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  } else if (user == "important") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = txt;
-    btn.className = "important";
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  } else if (user == "End") {
-    var btn = document.createElement("h2");
-    btn.className = "log-item";
-    btn.innerHTML = "C:\\User\\Input> " + txt;
-    btn.className = "End";
-    document.getElementById("log").appendChild(btn);
-    scrollToBottom();
-  }
-  if (blue) btn.style.color = "#3B7ECF";
 }
