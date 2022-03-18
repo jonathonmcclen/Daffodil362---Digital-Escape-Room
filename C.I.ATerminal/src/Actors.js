9b84dac208d0f85306609409a5ff79cf9fca79b2
--replace-all <jonathonsmcclendon@gmail.com>
2022-03-18 14:43:26 -0500

    actor hash
    holds name and html wraper for lines

---
+++ C.I.ATerminal/src/Actors.js
@@ -0,0 +1,6 @@
+const ACTORS {
+    comp: [`<h2 class="log-item"> C:/User/Input> ` , "</h2>"],
+    player: [`<h2 class="log-item"> C:/User/Input> ` , "</h2>"],
+    def: [`<h2 class="log-item">`, `</h2>`],
+    important: [`<h2 class="log-item">`, `</h2>`]
+}
