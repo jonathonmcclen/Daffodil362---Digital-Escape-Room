730ed0bd86f692807c4193aa8528b5c705b25b77
--replace-all <jonathonsmcclendon@gmail.com>
2022-03-18 20:07:36 -0500

    creating json objects for game section

---
+++ C.I.ATerminal/src/HackAndDecrypt/HackAndDecrypt.js
@@ -0,0 +1,258 @@
+class HackAndDecrypt {
+    let answers = { 
+        urls: [
+            { 
+                encrypted: FileLokPuzzle1En, 
+                decrypted: FileLokPuzzle1De, 
+                cutscene: [
+                    ["comp", "COMPLETE"],
+                    ["def", ".........................................................."],
+                    ["comp", "Authentication Code: 3512076170"],
+                    ["def", ".........................................................."],
+                    ["comp", "ENTER URL TO HACK OR DECYRYPT"],
+                    ["def", ".........................................................."],
+                ]
+            },
+            { encrypted: FileLokPuzzle2En,
+                decrypted: FileLokPuzzle2De,
+                cutscene: [
+                    ["comp", "COMPLETE"],
+                    ["def", ".........................................................."],
+                    ["comp", "SERVER IP: 19.087.125"],
+                    ["comp", "Authentication Code: 3259241426"],
+                    ["def", ".........................................................."],
+                    ["comp", "ENTER URL TO HACK OR DECYRYPT:"],
+                    ["def", ".........................................................."],
+                ]
+            },
+            { 
+                encrypted: FileLokPuzzle3En,
+                decrypted: FileLokPuzzle3De,
+                cutscene: [
+                    ["comp, COMPLETE"],
+                    ["comp, SERVER IP: 82.607.459"],
+                    ["comp, Authentication Code: That would be too easy :)"],
+                    ["comp, DECRYPTED URL:"],
+                    ["#showURL(URLPuzzleOne)"],
+                    ["comp, DECRYPTED URL's can be pasted into your browser"],
+                    ["def, .........................................................."],
+                    ["comp, ENTER URL TO HACK OR DECYRYPT:"],
+                    ["def, .........................................................."],
+                ]
+            },
+            { 
+                encrypted: FileLokPuzzle4En,
+                decrypted: FileLokPuzzle4De,
+                cutscene: [
+                    ["comp, COMPLETE"],
+                    ["comp, SERVER IP: 62.746.271"],
+                    ["comp, Passcode: 651697597)"],
+                    ["comp, DECRYPTED URL:"],
+                    ["#showURL(URLPuzzleTwo)"],
+                    ["comp, DECRYPTED URL's can be pasted into your browser."],
+                    ["def, .........................................................."],
+                    ["comp, ENTER URL TO HACK OR DECRYPT"],
+                    ["def, .........................................................."],
+                  ]
+                },
+            { 
+                encrypted: FileLokPuzzle5En,
+                decrypted: FileLokPuzzle5De,
+                cutscene: [
+                    ["comp, COMPLETE"],
+                    ["def, .........................................................."],
+                    ["comp, DECRYPTED URL:"],
+                    ["comp, https://twitter.com/Anonymo00841943"],
+                    ["comp, This URL is hackable"],
+                    ["comp, DECRYPTED URL's can be pasted into your browser"],
+                    ["def, .........................................................."],
+                    ["comp, ENTER URL TO HACK OR DECYRYPT"],
+                    ["def, .........................................................."],
+                  ]
+                },
+            { 
+                encrypted: FileLokPuzzle6En,
+                decrypted: FileLokPuzzle6De,
+                cutscene: [
+                    ["comp, COMPLETE"]
+                    ["def, .........................................................."]
+                    ["comp, Message1> One blue ball bounces."]
+                    ["comp, Message2> The red desk has four legs."]
+                    ["comp, Message3> I have nothing that is purple."]
+                    ["comp, Message4> A girl has green eyes."]
+                    ["comp, Message5> A yellow spider crawls."]
+                    ["comp, Message6> Three hard boiled eggs."]
+                    ["comp, Message7> Five oranges in a fruit bowl."]
+                    ["comp, Message8> Six grains of sand remain in the hour glass."]
+                    ["comp, Message9> Seven burnt matches."]
+                    ["comp, Message10> Nine peices of silver."]
+                    ["important, CHECK YOUR EMAIL."]
+                    ["important, IF YOU DIDNT RECIEVE AN EMAIL, VISIT:"]
+                    ["important, https://daffodil362.com/Email-2"]
+                    ["comp, https://escroomgames.com/Games/Daffodil362/Email-2, "]
+                    ["def, .........................................................."]
+                    ["comp, ENTER URL TO HACK OR DECRYPT:"]
+                    ["def, .........................................................."]
+                ]
+            },
+            { 
+                encrypted: FileLokPuzzle7En,
+                decrypted: FileLokPuzzle7De,
+                cutscene: [
+
+                ]
+            },
+            { 
+                encrypted: FileLokPuzzle8En,
+                decrypted: FileLokPuzzle8De,
+                cutscene: [
+
+                ]
+            },
+        ]
+    }
+}
+
+
+
+/*comp, Login Successful for user  + playerUsername
+comp, Your C.I.A terminal is now capable of Hacking and Decrypting URLs
+comp, ENCRYPTED URLs look like this:
+comp, *** EXAMPLE ****
+comp,https://75896-29742-69504-22231
+comp, All DECRYPTED URls can be used in a browser to visit web pages.
+comp, ENCRYPTED URL's MUST be DECRYPTED before pasting into browser address window.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECYRYPT:
+def, ..........................................................
+
+comp, COMPLETE
+def, ..........................................................
+comp, Decrypted URL:
+#showURL(URLPuzzleFive
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+
+comp, COMPLETE
+def, ..........................................................
+comp, Message1> FileLok Code: 6597
+comp, Message2> @Ax7> I forgot my username again!
+comp, Message3> @Anonymous5> Seriously? Here, I reset it...
+comp, Message4> @Anonymous5> Username: DontForgetAgain
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+
+comp, COMPLETE
+def, ..........................................................
+comp, Decrypted URL:
+#showURL(alabasterTerminalURL
+comp, Decrypted URLs can be pasted into your browser.
+comp, Some Decrypted URLs can be Hacked.
+comp, This URL IS hackable.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT:
+def, ..........................................................
+
+comp, Wait...
+comp, This is the EXAMPLE
+comp, VERY FUNNY Mr. Funny Guy
+def, ..........................................................
+comp, ENTER A REAL URL TO HACK OR DECRYPT
+def, ..........................................................
+
+comp, ERROR
+comp, STEP SKIPPED
+comp,Re-Read The Riddle
+comp,See if you can HACK IT
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT:
+def, ..........................................................
+
+comp, COMPLETE
+def, ..........................................................
+comp, Decrypted URL:
+showURL(FileLokPuzzle1De
+comp, Decrypted URLss can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL:
+showURL(FileLokPuzzle2De
+comp, Decrypted URL's can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle3De
+comp, Decrypted URLs can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle4De
+comp, Decrypted URLs can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT:
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle5De
+comp, Decrypted URLs can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle6De
+comp, Decrypted URLs can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle7De
+comp, Decrypted URLs can be pasted into your browser.
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT:
+def, ..........................................................
+Ready_For_Input(
+
+comp, COMPLETE
+success_sound.play(
+def, ..........................................................
+comp, Decrypted URL: 
+showURL(FileLokPuzzle8De
+comp, Decrypted URLs can be pasted into your browser
+def, ..........................................................
+comp, ENTER URL TO HACK OR DECRYPT
+def, ..........................................................
+Ready_For_Input(
